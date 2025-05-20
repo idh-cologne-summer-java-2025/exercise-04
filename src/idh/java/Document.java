8ccb1824d541d802881df1b7d35abfcaf7b726a3
chihayabu <chihayabu@proton.me>
2025-05-20 12:23:25 +0200

    Update Document.java

--- src/idh/java/Document.java
+++ src/idh/java/Document.java
@@ -1,64 +1,49 @@
 package idh.java;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Iterator;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Document implements Iterable<String> {
-	String documentText;
+    private String documentText;
 
-	public static Document readFromFile(File f) throws IOException {
-		FileReader fileReader = new FileReader(f);
-		int ch;
-		StringBuilder b = new StringBuilder();
-		while( (ch = fileReader.read()) != -1 ) {
-			b.append((char) ch);
-		}
-		fileReader.close();
-		Document doc = new Document();
-		doc.documentText = b.toString();
-		
-		return doc;
-	}
-	
-	public String getDocumentText() {
-		return documentText;
-	}
+    public static Document readFromFile(File file) throws IOException {
+        try (FileReader fr = new FileReader(file);
+             StringWriter sw = new StringWriter()) {
+            char[] buf = new char[4096];
+            int n;
+            while ((n = fr.read(buf)) != -1) sw.write(buf, 0, n);
+            Document doc = new Document();
+            doc.documentText = sw.toString();
+            return doc;
+        }
+    }
 
-	public void setDocumentText(String documentText) {
-		this.documentText = documentText;
-	}
-	
-	public static final void main(String[] args) throws IOException {
-		Document d = Document.readFromFile(new File("data/dracula.txt"));
-		int i = 0;
-		for (String token : d) {
-			System.out.println(i++ + ": " + token + " ");
-			if (i > 100)
-				break;
-		}
-	}
+    public String getDocumentText() {
+        return documentText;
+    }
 
-	@Override
-	public Iterator<String> iterator() {
-		return new Iterator<String>() {
+    public void setDocumentText(String text) {
+        this.documentText = text;
+    }
 
-			StringTokenizer tokenizer = new StringTokenizer(documentText);
-			
-			@Override
-			public boolean hasNext() {
-				return tokenizer.hasMoreTokens();
-			}
+    public double ttr() {
+        Set<String> types = new HashSet<>();
+        int tokens = 0;
+        for (String token : this) {
+            tokens++;
+            types.add(token);
+        }
+	    
+        return tokens == 0 ? 0 : (double) types.size() / tokens;
+    }
 
-			@Override
-			public String next() {
-				return tokenizer.nextToken();
-			}
-			
-		};
-	}
-	
-	
+    @Override
+    public Iterator<String> iterator() {
+        return Arrays.asList(documentText.split("\\s+")).iterator();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Document d = Document.readFromFile(new File("data/dracula.txt"));
+        System.out.println(d.ttr());
+    }
 }
