2524e48bcb49508532568e9c200437f4b619bb18
emil <emil@localhost>
2025-05-20 17:13:26 +0200

    Exercise 4 TTR

--- src/idh/java/Document.java
+++ src/idh/java/Document.java
@@ -3,7 +3,9 @@ package idh.java;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Document implements Iterable<String> {
@@ -13,16 +15,16 @@ public class Document implements Iterable<String> {
 		FileReader fileReader = new FileReader(f);
 		int ch;
 		StringBuilder b = new StringBuilder();
-		while( (ch = fileReader.read()) != -1 ) {
+		while ((ch = fileReader.read()) != -1) {
 			b.append((char) ch);
 		}
 		fileReader.close();
 		Document doc = new Document();
 		doc.documentText = b.toString();
-		
+
 		return doc;
 	}
-	
+
 	public String getDocumentText() {
 		return documentText;
 	}
@@ -30,35 +32,45 @@ public class Document implements Iterable<String> {
 	public void setDocumentText(String documentText) {
 		this.documentText = documentText;
 	}
+
 	
-	public static final void main(String[] args) throws IOException {
-		Document d = Document.readFromFile(new File("data/dracula.txt"));
-		int i = 0;
-		for (String token : d) {
-			System.out.println(i++ + ": " + token + " ");
-			if (i > 100)
-				break;
+	public double ttr() {
+		Set<String> types = new HashSet<>();
+		int tokenCount = 0;
+
+		for (String token : this) {
+			types.add(token);
+			tokenCount++;
 		}
+
+		if (tokenCount == 0) {
+			return 0.0;
+		}
+
+		return (double) types.size() / tokenCount;
 	}
 
-	@Override
+	public static void main(String[] args) throws IOException {
+		Document d = Document.readFromFile(new File("data/dracula.txt"));
+		System.out.println("TTR: " + d.ttr());
+	}
+
+
 	public Iterator<String> iterator() {
 		return new Iterator<String>() {
 
 			StringTokenizer tokenizer = new StringTokenizer(documentText);
-			
-			@Override
+
+	
 			public boolean hasNext() {
 				return tokenizer.hasMoreTokens();
 			}
 
-			@Override
+		
 			public String next() {
 				return tokenizer.nextToken();
 			}
-			
+
 		};
 	}
-	
-	
 }
